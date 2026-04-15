a15867e970d320fa6664a07c3fcb42295b04877e
Daniel Espinosa <despinosa@iescomercio.com>
2026-04-15 12:44:10 +0200

    Add main file with javadoc info

---
+++ prueba.java
@@ -0,0 +1,15 @@
+/**
+ * la clase que tal...
+ */
+
+public class prueba {
+
+    /**
+     * El método que
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
